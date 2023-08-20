5db7ce1429d4e4af8a67c27a44a1d9733788aead
takusan23 <takumu1200@gmail.com>
2023-08-21 02:51:18 +0900

    targetSdk を更新したので PlayStore に公開する

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "io.github.takusan23.mymusiccontrolwidget"
         minSdk = 21
         targetSdk = 33
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
